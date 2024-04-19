8e0185dd1ebf4df2684dcec901c7920026efdf22
Rdxking345 <167410790+Rdxking345@users.noreply.github.com>
2024-04-20 00:10:44 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>our app ritik !</Text>
       <StatusBar style="auto" />
     </View>
   );
